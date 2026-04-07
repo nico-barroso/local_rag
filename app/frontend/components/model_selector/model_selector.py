475123b0d5f0d9ef11302819ffbfae124805d634
Nico <josenicolasbarrosogarcia@gmail.com>
2026-04-07 17:06:45 +0100

    feature(model): added the capability to choose between responses models and the top-k

--- app/frontend/components/model_selector/model_selector.py
+++ app/frontend/components/model_selector/model_selector.py
@@ -1,15 +1,26 @@
 import streamlit as st
-from rag.embeddings.embedder import gemma3_4b, gemma3_12b
+from frontend.utils.utils import styles_file_opener
+from rag.embeddings.initialize_embedding import llm_model
 
 MODELS = {
-    "gemma3:4b": gemma3_4b,
-    "gemma3:12b": gemma3_12b,
+    "gemma3:4b": lambda: llm_model("gemma3:4b"),
+    "gemma3:12b": lambda: llm_model("gemma3:12b"),
 }
+
+MODEL_STYLES = f"""
+<style>
+{styles_file_opener(__file__)}
+</style>
+"""
 
 
 def model_selector():
     """Controls the selectors of the LLM model and the top-k retrieved. DEV-NOTE: using the reranker retrieves by default 3 nodes,
-    adding more nodes to the top-k just adds more nodes to the ranking."""
+    adding more nodes to the top-k just adds more nodes to the ranking.
+    Note:
+        Adds the top-k and the model to the streamlit session_state."""
+    st.markdown(MODEL_STYLES, unsafe_allow_html=True)
+
     if "top-k" not in st.session_state:
         st.session_state["top-k"] = "10"
     if "model" not in st.session_state:
