b0686151a0be7ac68eb6aef33b43eb52ce482d57
Nico <josenicolasbarrosogarcia@gmail.com>
2026-04-08 16:57:06 +0100

    fix(watcher): fixed watcher indexing and speed.
    Fixed a bug with store.py VectorStore(num_workers) if from another
    class, this param is not working here
    
    Just with the default settings works takes 162.51 seconds to index 6
    files (20.89 nodes per second)

--- app/rag/corpus/watcher.py
+++ app/rag/corpus/watcher.py
@@ -1,4 +1,8 @@
 import time
+from pathlib import Path
+from typing import Any
+
+from llama_index.core.indices.vector_store.base import VectorStoreIndex
 from rag.chunks.splitter import document_splitter
 from rag.corpus.reader import reader
 from watchdog.events import FileSystemEventHandler
@@ -6,32 +10,46 @@ from watchdog.observers import Observer
 
 
 class DocHandler(FileSystemEventHandler):
-    def __init__(self, index):
+    """Event handler for file system events in the document indexing process.
+
+    This class inherits from FileSystemEventHandler and defines the logics which triggers
+    when new files are detected in the observed directory.
+    """
+
+    def __init__(self, index: Any):
+        """Initialize the handler with the specific index."""
         self.index = index
 
     def on_created(self, event):
+        """It executes when a new file or directory is created.
+
+        Detects new files, extracts their content, split them into nodes
+        and insert them in the index automatically.
+        """
         if event.is_directory:
             return
 
-        print(f"Nuevo archivo detectado: {event.src_path}")
+        time.sleep(1)
+        print(f"Watcher: New file detected -> {event.src_path}")
 
         documents = reader(input_files=[event.src_path])
         nodes = document_splitter(documents)
-        self.index.insert_nodes(nodes)
-        print(f"Indexados {len(nodes)} nodos de {event.src_path}")
+        self.index.insert_nodes(nodes, show_progress=True)
+
+        print(f"Indexed {len(nodes)} nodes of {event.src_path}")
 
 
-def start_watcher(index, path):
+def start_watcher(index: VectorStoreIndex, path: str):
+    """Starts an observer in the background to monitor a directory.
+
+    Args:
+        index (VectorStoreIndex): The index object where the nodes are going to be located.
+        path(Str): Path of the folder to observe.
+    """
     handler = DocHandler(index)
     observer = Observer()
-    observer.schedule(handler, path=path, recursive=False)
+    absolute_path = str(Path(path).absolute())
+
+    observer.schedule(handler, path=absolute_path, recursive=False)
     observer.start()
-    print(f"Watching '{path}'...")
-
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-
-    observer.join()
+    print(f"✅ Watcher started and monitoring in : {absolute_path}")
